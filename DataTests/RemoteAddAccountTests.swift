adba61ae03214e519e237d7edceb7033561efb8d
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-28 15:15:04 -0300

    move sut creation to a factory helper method

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -22,35 +22,40 @@ protocol HttpPostClient {
 
 class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_http_client_with_correct_url() throws {
-        let url = URL(string: "https://any-url.com")!
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        let dto = AddAccountDTO(
-            name: "any_name",
-            email: "any_email",
-            password: "any_password",
-            passwordConfirmation: "any_password"
-        )
-        sut.add(data: dto)
+        let url = URL(string: "https://any-other-url.com")!
+        let (sut, httpClientSpy) = makeSut(url: url)
+        sut.add(data: makeAddAccountDTO())
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
     func test_add_should_call_http_client_with_correct_values() throws {
-        let httpClientSpy = HttpClientSpy()
-        let sut = RemoteAddAccount(url: URL(string: "https://any-url.com")!, httpClient: httpClientSpy)
-        let dto = AddAccountDTO(
-            name: "any_name",
-            email: "any_email",
-            password: "any_password",
-            passwordConfirmation: "any_password"
-        )
-        sut.add(data: dto)
+        let (sut, httpClientSpy) = makeSut()
+        let dto = makeAddAccountDTO()
+        sut.add(data: makeAddAccountDTO())
         let data = try? JSONEncoder().encode(dto)
         XCTAssertEqual(httpClientSpy.data, data)
     }
 }
 
 extension RemoteAddAccountTests {
+    func makeSut(url: URL = URL(string: "https://any-url.com")!) -> (
+        sut: RemoteAddAccount,
+        httpClientSpy: HttpClientSpy
+    ) {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        return (sut, httpClientSpy)
+    }
+    
+    func makeAddAccountDTO() -> AddAccountDTO {
+        return AddAccountDTO(
+            name: "any_name",
+            email: "any_email",
+            password: "any_password",
+            passwordConfirmation: "any_password"
+        )
+    }
+    
     class HttpClientSpy: HttpPostClient {
         var url: URL?
         var data: Data?
