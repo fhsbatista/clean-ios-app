5af9091495658d228a58e913a122f84abeb2ad74
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-28 15:05:15 -0300

    ensure RemoteAddAccount calls HttpClient with correct data

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Domain
 
 class RemoteAddAccount {
     private let url: URL
@@ -9,32 +10,54 @@ class RemoteAddAccount {
         self.httpClient = httpClient
     }
     
-    func add() {
-        httpClient.post(url: url)
+    func add(data: AddAccountDTO) {
+        let data = try? JSONEncoder().encode(data)
+        httpClient.post(to: url, with: data)
     }
 }
 
 protocol HttpPostClient {
-    func post(url: URL)
+    func post(to: URL, with: Data?)
 }
 
 class RemoteAddAccountTests: XCTestCase {
-
     func test_add_should_call_http_client_with_correct_url() throws {
         let url = URL(string: "https://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        sut.add()
+        let dto = AddAccountDTO(
+            name: "any_name",
+            email: "any_email",
+            password: "any_password",
+            passwordConfirmation: "any_password"
+        )
+        sut.add(data: dto)
         XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    func test_add_should_call_http_client_with_correct_values() throws {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: URL(string: "https://any-url.com")!, httpClient: httpClientSpy)
+        let dto = AddAccountDTO(
+            name: "any_name",
+            email: "any_email",
+            password: "any_password",
+            passwordConfirmation: "any_password"
+        )
+        sut.add(data: dto)
+        let data = try? JSONEncoder().encode(dto)
+        XCTAssertEqual(httpClientSpy.data, data)
     }
 }
 
 extension RemoteAddAccountTests {
     class HttpClientSpy: HttpPostClient {
         var url: URL?
+        var data: Data?
         
-        func post(url: URL) {
+        func post(to url: URL, with data: Data?) {
             self.url = url
+            self.data = data
         }
     }
 }
